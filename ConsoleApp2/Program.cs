f213e0debdf57269e9c32ba6330dbc472ddc98e8
Gyana9 <GYANA.SAI@lntinfotech.com>
2020-07-28 15:35:54 +0530

    commitOne

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine(num1);
+
+            }
+            Console.WriteLine("HI");
+        }
+    }
+}
